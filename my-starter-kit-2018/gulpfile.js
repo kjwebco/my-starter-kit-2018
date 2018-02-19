ae690a7f696a6cec513d44e7ad6111c1d2c248f1
Kevin Jendreas <kjendreas@gmail.com>
2018-02-18 23:16:50 -0800

    Add files via upload

--- my-starter-kit-2018/gulpfile.js
+++ my-starter-kit-2018/gulpfile.js
@@ -4,8 +4,8 @@ var autoprefixer = require('gulp-autoprefixer');
 // const concat = require('gulp-concat')
 // const babel = require('gulp-babel')
 // const watch = require('gulp-watch')
-// var browserSync = require('browser-sync')
-// var reload = browserSync.reload
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 // var shell = require('gulp-shell')
 
 
@@ -15,9 +15,17 @@ gulp.task('sass', function () {
       .pipe(autoprefixer({
         browsers: ['last 2 versions']
       }))
-      .pipe(gulp.dest('./public/css'));
+      .pipe(gulp.dest('./public/css'))
+      .pipe(browserSync.stream())
   });
+gulp.task('browser-sync', function(){
+  browserSync.init({
+    server: './public',
+    notify: false,
+    open: true
+  })
+})
 
-gulp.task('default', ['sass'], function(){
+gulp.task('default', ['sass', 'browser-sync'], function(){
   gulp.watch('./src/scss/**/*', ['sass'])
 })
